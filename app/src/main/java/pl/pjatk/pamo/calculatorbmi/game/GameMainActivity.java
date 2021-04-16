00af224cb61eac2284bdc70217890c89494a2708
dominiqxiqx <domste.domste@gmail.com>
2021-04-16 15:09:58 +0200

    SurfaceView listener correction

--- app/src/main/java/pl/pjatk/pamo/calculatorbmi/game/GameMainActivity.java
+++ app/src/main/java/pl/pjatk/pamo/calculatorbmi/game/GameMainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import pl.pjatk.pamo.calculatorbmi.R;
+
 public class GameMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_main);
     }
+
+
 }
