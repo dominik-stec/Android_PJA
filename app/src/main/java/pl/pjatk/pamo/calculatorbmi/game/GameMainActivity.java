6116611412533b13e9a0dea5c8a22529e14e36d4
dominiqxiqx <domste.domste@gmail.com>
2021-04-16 16:11:22 +0200

    adaptive game from 'P. Deitel Android 6 for Programmers' book

--- app/src/main/java/pl/pjatk/pamo/calculatorbmi/game/GameMainActivity.java
+++ app/src/main/java/pl/pjatk/pamo/calculatorbmi/game/GameMainActivity.java
@@ -1,5 +1,6 @@
 package pl.pjatk.pamo.calculatorbmi.game;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,12 @@ import pl.pjatk.pamo.calculatorbmi.R;
 
 public class GameMainActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
 
