8e8026f0339f8017e67adf4c1f1dbea474498e06
dominiqxiqx <domste.domste@gmail.com>
2021-05-13 09:31:36 +0200

    change from java to kotlin into game.GameMainActivity.kt

---
+++ app/src/main/java/pl/pjatk/pamo/kotlincalculatorbmi/game/GameMainActivity.kt
@@ -0,0 +1,37 @@
+package pl.pjatk.pamo.kotlincalculatorbmi.game
+
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.pjatk.pamo.calculatorbmi.R
+
+
+class GameMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    }
+}
+
+//
+//import android.content.pm.ActivityInfo;
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import pl.pjatk.pamo.calculatorbmi.R;
+//
+//public class GameMainActivity extends AppCompatActivity {
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_game_main);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//    }
+//
+//
+//}
