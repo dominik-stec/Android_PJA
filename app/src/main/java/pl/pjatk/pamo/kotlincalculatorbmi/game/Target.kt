4af5c02cd4d50451347ed44b1c4336ffac3146a8
dominiqxiqx <domste.domste@gmail.com>
2021-05-13 09:33:41 +0200

    change from java to kotlin into game.Target.kt

---
+++ app/src/main/java/pl/pjatk/pamo/kotlincalculatorbmi/game/Target.kt
@@ -0,0 +1,61 @@
+// Target.java
+// Subclass of GameElement customized for the Target
+package pl.pjatk.pamo.kotlincalculatorbmi.game
+
+
+class Target     // constructor
+    (
+    view: CannonView?, color: Int, // returns the hit reward for this Target
+    val hitReward // the hit reward for this target
+    : Int, x: Int, y: Int,
+    width: Int, length: Int, velocityY: Float
+) :
+    GameElement(
+        view!!, color, CannonView.TARGET_SOUND_ID, x, y, width * 4, length,
+        velocityY
+    )
+
+/*********************************************************************************
+ * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and * Pearson Education, *
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this   *
+ * book have used their * best efforts in preparing the book. These efforts      *
+ * include the * development, research, and testing of the theories and programs *
+ * * to determine their effectiveness. The authors and publisher make * no       *
+ * warranty of any kind, expressed or implied, with regard to these * programs   *
+ * or to the documentation contained in these books. The authors * and publisher *
+ * shall not be liable in any event for incidental or * consequential damages in *
+ * connection with, or arising out of, the * furnishing, performance, or use of  *
+ * these programs.                                                               *
+ *********************************************************************************/
+
+
+//
+//public class Target extends pl.pjatk.pamo.calculatorbmi.game.GameElement {
+//   private int hitReward; // the hit reward for this target
+//
+//   // constructor
+//   public Target(CannonView view, int color, int hitReward, int x, int y,
+//      int width, int length, float velocityY) {
+//      super(view, color, CannonView.TARGET_SOUND_ID, x, y, width*4, length,
+//         velocityY);
+//      this.hitReward = hitReward;
+//   }
+//
+//   // returns the hit reward for this Target
+//   public int getHitReward() {
+//      return hitReward;
+//   }
+//}
+//
+///*********************************************************************************
+// * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and * Pearson Education, *
+// * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this   *
+// * book have used their * best efforts in preparing the book. These efforts      *
+// * include the * development, research, and testing of the theories and programs *
+// * * to determine their effectiveness. The authors and publisher make * no       *
+// * warranty of any kind, expressed or implied, with regard to these * programs   *
+// * or to the documentation contained in these books. The authors * and publisher *
+// * shall not be liable in any event for incidental or * consequential damages in *
+// * connection with, or arising out of, the * furnishing, performance, or use of  *
+// * these programs.                                                               *
+// *********************************************************************************/
